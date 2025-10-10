a1b04fd5ce36246d95e8ffcdec3e127e29067130
Thawshi Srikanth <thawshi.dev@protonmail.com>
2025-10-10 07:37:54 +0530

    refactor: improve code formatting and remove unnecessary whitespace in Who We Are section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import Chapters from "@/components/sections/home-page/chapters";
 import DivisionsSection from "@/components/sections/home-page/division/divisionsection";
 import WhoWeAreSection from "@/components/sections/home-page/who-we-are/whowearesection";
 
-
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center">
