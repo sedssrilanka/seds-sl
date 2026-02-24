7bcc7a87093361ffeb430197dfe829da29e84fb1
Thawshi-Srikanth <thawshi.dev@protonmail.com>
2026-02-24 08:37:28 +0530

    feat: Configure home page revalidation to 60 seconds.

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -6,6 +6,8 @@ import FAQSection from "@/components/sections/home-page/faqs/faqsection";
 import WhoWeAreSection from "@/components/sections/home-page/who-we-are/whowearesection";
 import ContactSection from "@/components/sections/home-page/contact/contact-section";
 
+export const revalidate = 60; // Revalidate every 60 seconds
+
 export default function Home() {
   return (
     <div>
